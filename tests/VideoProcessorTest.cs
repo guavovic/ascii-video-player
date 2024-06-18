5dcfffa68872e460c7c1f748951ff4f462851153
guavovic <99424489+guavovic@users.noreply.github.com>
2024-06-18 12:54:38 -0300

    create project

---
+++ tests/VideoProcessorTest.cs
@@ -0,0 +1,37 @@
+﻿//using Emgu.CV;
+
+//namespace VideoToAsciiConverter.tests
+//{
+//    internal class VideoProcessorTest
+//    {
+//        static void Main(string[] args)
+//        {
+//            const string videoPath = @"C:\Users\gusta\Documents\GitHub\video-to-ascii-converter\teste.mp4";
+//            VideoCapture cap = new VideoCapture(videoPath);
+
+//            if (!cap.IsOpened)
+//            {
+//                Console.WriteLine("Erro ao abrir o vídeo!");
+//                return;
+//            }
+
+//            Mat frame = new Mat();
+
+//            while (true)
+//            {
+//                cap.Read(frame);
+
+//                if (frame.IsEmpty)
+//                    break;
+
+//                CvInvoke.Imshow("Frame", frame);
+
+//                if (CvInvoke.WaitKey(30) == 27) // Pressione ESC para sair
+//                    break;
+//            }
+
+//            cap.Dispose();
+//            CvInvoke.DestroyAllWindows();
+//        }
+//    }
+//}
